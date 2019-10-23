4f070db2a40a8b6c2bea7efd3f7673ae9ccd4109
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-23 16:43:37 -0700

    Add a student, render a success page and link to student's info page

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,21 +1,49 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, session
 
 import hackbright
 
 app = Flask(__name__)
+
+app.secret_key = 'this-should-be-something-unguessable'
+
+# @app.route("/")
+# def show_homepage():
+#     return render_template("homepage.html")
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("student-add.html", github=github)
 
 
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    # return "{} is the GitHub account for {} {}".format(github, first, last)
+
+    html = render_template("student_info.html", first=first, last=last, github=github)
+
+    return html
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
